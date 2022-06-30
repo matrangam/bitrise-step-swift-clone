de8470c9360c8d891d4b313f9c036a74876a31dd
Michael Matranga <michael.matranga@bitrise.io>
2022-06-30 13:59:15 -0400

    Removed boiler plate.

--- Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
+++ Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
@@ -2,11 +2,7 @@ import GitKit
 
 @main
 public struct bitrise_step_swift_test {
-    public private(set) var text = "Hello, World!"
-
     public static func main() {
-        print(bitrise_step_swift_test().text)
-        
         let git = Git(path: "$PWD")
         do {
             let output = try git.run(.cmd(.status))            
