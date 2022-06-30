093ec54cc231cf95306891fe916df36530a617a6
Michael Matranga <michael.matranga@bitrise.io>
2022-06-30 13:54:57 -0400

    Adding a i Adding a simple wrapper for git.

--- Package.swift
+++ Package.swift
@@ -8,13 +8,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/binarybirds/git-kit", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "bitrise-step-swift-test",
-            dependencies: []),
+            dependencies: [
+                .product(name: "GitKit", package: "git-kit"),
+            ]),
         .testTarget(
             name: "bitrise-step-swift-testTests",
             dependencies: ["bitrise-step-swift-test"]),
--- Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
+++ Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
@@ -1,8 +1,18 @@
+import GitKit
+
 @main
 public struct bitrise_step_swift_test {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
         print(bitrise_step_swift_test().text)
+        
+        let git = Git(path: "$PWD")
+        do {
+            let output = try git.run(.cmd(.status))            
+            print(output)
+        } catch {
+            print(error)
+        }
     }
 }
