1ed25b709483c9f7c31ecfd549173f927cf75275
Michael Matranga <michael.matranga@bitrise.io>
2022-07-08 13:17:57 -0400

    Adding config.

--- Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
+++ Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
@@ -1,8 +1,13 @@
+import Foundation
 import GitKit
 
 @main
 public struct bitrise_step_swift_test {
     public static func main() {
+        let env = ProcessInfo.processInfo.environment
+        let repoURL = env["REPOSITORY_URL"]
+        print("HEYO: \(repoURL ?? "NOEN")")
+        
         let git = Git(path: "$PWD")
         do {
             let output = try git.run(.cmd(.status))            
--- Tests/bitrise-step-swift-testTests/bitrise_step_swift_testTests.swift
+++ Tests/bitrise-step-swift-testTests/bitrise_step_swift_testTests.swift
@@ -6,6 +6,5 @@ final class bitrise_step_swift_testTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(bitrise_step_swift_test().text, "Hello, World!")
     }
 }
