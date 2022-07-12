00ce931f235f220d34ad7999341367676cce2a5d
Michael Matranga <michael.matranga@bitrise.io>
2022-07-12 11:00:50 +0200

    Better API; splitting code to add tests.

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/binarybirds/git-kit", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-tools-support-core", .upToNextMajor(from: "0.2.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -17,6 +18,7 @@ let package = Package(
             name: "bitrise-step-swift-test",
             dependencies: [
                 .product(name: "GitKit", package: "git-kit"),
+                .product(name: "SwiftToolsSupport-auto", package: "swift-tools-support-core")
             ]),
         .testTarget(
             name: "bitrise-step-swift-testTests",
--- Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
+++ Sources/bitrise-step-swift-test/bitrise_step_swift_test.swift
@@ -1,19 +1,78 @@
 import Foundation
 import GitKit
+import TSCBasic
 
 @main
 public struct bitrise_step_swift_test {
     public static func main() {
-        let env = ProcessInfo.processInfo.environment
-        let repoURL = env["REPOSITORY_URL"]
-        print("HEYO: \(repoURL ?? "NONE")")
-        
-        let git = Git(path: "$PWD")
+        guard let config = parseConfig() else {
+            print("Unable to parse config")
+            return
+        }
+        guard let clonePath = createClonePath(config.cloneDest) else {
+            print("Unable to create clone path")
+            return
+        }
+
+        let git = Git(path: clonePath.pathString)
+        let gitCommand = buildGitCommand(config: config)
+        print("YOOOOOOOOOOO: \(gitCommand)")
         do {
-            let output = try git.run(.cmd(.status))            
+            let output = try git.run(.raw(gitCommand))
             print(output)
         } catch {
             print(error)
         }
     }
+}
+
+func buildGitCommand(config: Config) -> String {
+    var commands: [String] = ["clone", config.repoURL, "-v"]
+    if let checkoutBranch = config.checkoutBranch {
+        commands.append("--branch \(checkoutBranch)")
+    }
+    if let cloneDepth = config.cloneDepth {
+        commands.append("--depth \(cloneDepth)")
+    }
+
+    return commands.joined(separator: " ")
+    
+}
+
+func createClonePath(_ cloneDest: String) -> AbsolutePath? {
+    let currentPath = AbsolutePath(FileManager.default.currentDirectoryPath)
+    let clonePath = currentPath.appending(component: cloneDest)
+    do {
+        try localFileSystem.createDirectory(clonePath)
+    } catch {
+        print("Clone path not created: \(error)")
+        return nil
+    }
+    return clonePath
+}
+
+func parseConfig() -> Config? {
+    let env = ProcessInfo.processInfo.environment
+    guard let repoURL = env["repository_url"] else {
+        return nil
+    }
+    guard let cloneDest = env["clone_destination"] else {
+        return nil
+    }
+    let checkoutBranch = env["checkout_branch"]
+    let cloneDepth = env["clone_depth"]
+    
+    return Config(
+        repoURL: repoURL,
+        cloneDest: cloneDest,
+        checkoutBranch: checkoutBranch,
+        cloneDepth: cloneDepth
+    )
+}
+
+struct Config {
+    let repoURL: String
+    let cloneDest: String
+    let checkoutBranch: String?
+    let cloneDepth: String?
 }
